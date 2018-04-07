9d53b101b470a1062fc91d727a82cf61907f2e77
harsh-a1 <harsh.atal@gmail.com>
2018-04-07 21:57:17 +0700

    attemp at tree

--- components/app.js
+++ components/app.js
@@ -1,11 +1,76 @@
 import React,{propTypes} from 'react';
 
 export function UploadFile(props){
-        return (
-                <div>
-                    <label>Upload .xlsx/csv file</label>
-                    <input type="file" id="fileInput"/>
-                    <button onClick={props.onClick}>Import</button>
-                </div>
+    return (
+            <div>
+            <label>Upload .xlsx/csv file</label>
+            <input type="file" id="fileInput"/>
+            <button onClick={props.onClick}>Import</button>
+            </div>
+    )
+}
+
+
+export function TreeComponent(props){
+
+    var instance = Object.create(React.Component.prototype)
+
+    
+    instance.props = props;   
+    var toggle = function(){
+        instance.setState(props.data)
+    }
+    instance.render = function(){
+        return <ul>
+            <Tree data={props.data} toggle={toggle}/>
+                </ul>
+    }
+
+    return instance;
+
+    
+    function Tree(props){
+
+       
+        if (!props.data.children){
+            return (
+                    <li>
+                    <LeafNode data={props.data} toggle = {props.toggle}/>                
+                    </li>
             )
+        }
+        
+        return  (            
+                <li><LeafNode data={props.data} toggle = {toggle} />
+                <ul>{
+                    props.data.children.map(function(child){
+                        return <Tree data={child} />
+                    })
+                }
+            </ul></li>
+        )
+        
+        function LeafNode(props){
+
+            function toggle(){
+
+                props.data.showChildren = !props.data.showChildren;
+                props.toggle();
+            }
+            
+            var toggleImg = "";
+
+            if (props.data.children){
+                toggleImg = props.data.showChildren  ?"./images/colapse.png":"./images/expand.png"; 
+            }            
+            return (
+                    <div>
+                    <span className="toggle"  >
+                    <img width="9" height="9" src={toggleImg} onClick={toggle} />
+                    </span>
+                    <a href="">{props.data.name}</a>
+                    </div>
+            )
+        }   
+    }
 }
